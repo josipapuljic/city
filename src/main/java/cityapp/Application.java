13c1e4e72b3eeaaeb6a7b61d9689b53803b659c6
Josipa Puljić <jsabljak@gmail.com>
2019-12-01 21:59:23 +0100

    Removed unused code (Firebase)

--- src/main/java/cityapp/Application.java
+++ src/main/java/cityapp/Application.java
@@ -8,10 +8,6 @@ import java.io.IOException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.google.auth.oauth2.GoogleCredentials;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseOptions;
-
 import repository.Repository;
 
 @SpringBootApplication
@@ -20,25 +16,6 @@ public class Application {
   public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 		
-		FileInputStream refreshToken;
-		try {
-			refreshToken = new FileInputStream("userauthentication-6887.json");
-			FirebaseOptions options = new FirebaseOptions.Builder()
-				    .setCredentials(GoogleCredentials.fromStream(refreshToken))
-				    .build();
-
-			//if (FirebaseApp.getApps().isEmpty()) {
-				FirebaseApp.initializeApp(options);
-			//}
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
 		Repository.init();
-
   }
 }
